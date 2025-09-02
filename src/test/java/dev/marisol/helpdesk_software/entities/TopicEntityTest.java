591dd2b3760effd82b6dcbf6e8a403a6b98816d1
Marisol-Mancera <marisolmancera10@gmail.com>
2025-09-02 17:56:53 +0200

    HSSA-11 test(request-entity): add unit test for initialization and accessors

--- src/test/java/dev/marisol/helpdesk_software/entities/TopicEntityTest.java
+++ src/test/java/dev/marisol/helpdesk_software/entities/TopicEntityTest.java
@@ -1,1 +1,24 @@
+package dev.marisol.helpdesk_software.entities;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Test;
+
+public class TopicEntityTest {
+
+    @Test
+    public void shouldInitializeTopicWithSetters (){
+
+        TopicEntity t = new TopicEntity();
+
+        t.setId(1L);
+        t.setName("Hardware");
+        t.setActive(true);
+
+        assertThat(t.getId(), is(equalTo(1L)));
+        assertThat(t.getName(),is(equalTo("Hardware")));
+        assertThat(t.isActive(), is(true));
+    }
+}
